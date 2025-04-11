04583593393529d71fb35089513aaefc94c6c0e0
vresian <vresian@proton.me>
2025-04-11 16:40:39 +0200

    Set up a basic adw window display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use adw::prelude::*;
+use adw::{Application, ApplicationWindow};
+
+const APP_ID: &'static str = "com.github.vresian.harmony";
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Harmony")
+        .build();
+
+    window.present();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id(APP_ID)
+        .build();
+
+    app.connect_activate(build_ui);
+
+    app.run();
 }
