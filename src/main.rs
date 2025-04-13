a1caf2e2fd6d9ddccc3b7fbc09c27c5b88ba1ae6
vresian <vresian@proton.me>
2025-04-13 10:58:02 +0200

    Connected the stylesheet

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod ui;
 
 use ui::login_window::LoginWindow;
 use adw::prelude::*;
-use adw::{Application, ApplicationWindow, gio};
+use adw::{Application, ApplicationWindow, gio, gtk::{CssProvider, gdk}};
 
 const APP_ID: &'static str = "com.github.vresian.harmony";
 
@@ -12,13 +12,24 @@ fn build_ui(app: &Application) {
     window.present();
 }
 
+fn load_css() {
+    let provider = CssProvider::new();
+    provider.load_from_string(include_str!("css/login_window.css"));
+
+    gtk::style_context_add_provider_for_display(
+        &gdk::Display::default().expect("Could not connect to a display"), 
+        &provider, adw::gtk::STYLE_PROVIDER_PRIORITY_APPLICATION
+    );
+}
+
 fn main() {
     gio::resources_register_include!("harmony.gresource").expect("Failed to register resources");
 
     let app = Application::builder()
         .application_id(APP_ID)
         .build();
-
+    
+    app.connect_startup(|_| load_css());
     app.connect_activate(build_ui);
 
     app.run();
