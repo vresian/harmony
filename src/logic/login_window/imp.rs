e468a9f3596b4ed5bd154d45b357109801f1f2e6
vresian <vresian@proton.me>
2025-04-13 21:17:17 +0200

    Created a very retarded way of token validation

--- src/logic/login_window/imp.rs
+++ src/logic/login_window/imp.rs
@@ -1,6 +1,16 @@
+use std::sync::OnceLock;
+use glib::clone;
 use adw::prelude::*;
 use adw::subclass::prelude::*;
-use gtk::{glib::{self, subclass::InitializingObject, GString}, CompositeTemplate};
+use gtk::{glib::{self, subclass::InitializingObject, GString}, CompositeTemplate, gio};
+use tokio::runtime::Runtime;
+
+fn runtime() -> &'static Runtime {
+    static RUNTIME: OnceLock<Runtime> = OnceLock::new();
+    RUNTIME.get_or_init(|| {
+        Runtime::new().expect("Setting up tokio runtime needs to succeed.")
+    })
+}
 
 #[derive(CompositeTemplate, Default)]
 #[template(resource = "/com/github/vresian/harmony/login_window.ui")]
@@ -35,8 +45,8 @@ impl LoginWindow {
         
         let classes = self.token_entry.get().css_classes();
 
-        let mut classes_str: Vec<&str> = classes.
-            iter()
+        let mut classes_str: Vec<&str> = classes
+            .iter()
             .map(|gstring| gstring.as_str())
             .filter(|string| string != &"error")
             .collect();
@@ -47,7 +57,44 @@ impl LoginWindow {
         }
 
         self.token_entry.set_css_classes(classes_str.as_slice());
-        println!("{}", self.token_entry.get().text());
+        
+        let (sender, receiver) = async_channel::bounded(1);
+        
+        runtime().spawn(clone!(
+            #[strong]
+            sender,
+            async move {
+                let client = reqwest::Client::new();
+
+                let response = client
+                    .get("https://discord.com/api/v9/users/@me")
+                    .header(reqwest::header::AUTHORIZATION, token)
+                    .send()
+                    .await;
+
+                sender
+                    .send(response)
+                    .await
+                    .expect("The channel needs to be open.");
+            }
+        ));
+            
+        let entry = self.token_entry.get();
+
+        glib::spawn_future_local(async move {
+            while let Ok(response) = receiver.recv().await {
+                if let Ok(response) = response {
+                    if response.status().is_client_error() {
+                        let classes = entry.css_classes();
+                        let mut str_classes: Vec<&str> = classes.iter().map(|gstring| gstring.as_str()).collect();
+                        str_classes.push(&"error");
+                        entry.set_css_classes(&str_classes);
+                    }
+                } else {
+                    println!("Could not make a `GET` request.");
+                }
+            }
+        });
     }
 }
 
